0b2025054181d8cb55e748b155c0937ad0016655
Julia Mayer <julia-stephanie.mayer@charite.de>
2026-01-06 10:58:54 +0100

    Changes for clarity

--- deterministic SIR odin.R
+++ deterministic SIR odin.R
@@ -5,7 +5,7 @@ initial(R) <- 0
 initial(incidence, zero_every = 1) <- 0 # incidence is reset to zero at each time step
 
 # Core equations for transitions between compartments:
-update(S) <-  S - n_SI * dt
+update(S) <- S - n_SI * dt
 update(I) <- I + n_SI * dt - n_IR * dt
 update(R) <- R + n_IR * dt
 update(incidence) <- incidence + n_SI * dt
@@ -14,14 +14,14 @@ update(incidence) <- incidence + n_SI * dt
 n_SI <- beta * S * I / N # S to I
 n_IR <- gamma * I # I to R
 
-# User defined parameters - you can add default values in parentheses and they 
+# User defined parameters - you can add default values in parentheses and they
 # can be changed when initialising the model:
 beta <- parameter()
 gamma <- parameter()
 I0 <- parameter()
-N <- 1000
+N <- parameter(1000)
 
-# Comparison to data - this is used for the likelihood function when fitting to the data. 
+# Comparison to data - this is used for the likelihood function when fitting to the data.
 # We're telling odin that the cases can be found in the data and that they
 # should follow a Poisson distribution. They are compared with the incidence output of our model.
 cases <- data()
--- stochastic SIR odin.R
+++ stochastic SIR odin.R
@@ -18,14 +18,14 @@ n_IR <- Binomial(I, p_IR) # I to R
 p_SI <- 1 - exp(-beta * I / N * dt)
 p_IR <- 1 - exp(-gamma * dt)
 
-# User defined parameters - you can add default values in parentheses and they 
+# User defined parameters - you can add default values in parentheses and they
 # can be changed when initialising the model:
 beta <- parameter()
 gamma <- parameter()
 I0 <- parameter()
-N <- 1000
+N <- parameter(1000)
 
-# Comparison to data - this is used for the likelihood function when fitting to the data. 
+# Comparison to data - this is used for the likelihood function when fitting to the data.
 # We're telling odin that the cases can be found in the data and that they
 # should follow a Poisson distribution. They are compared with the incidence output of our model.
 cases <- data()
